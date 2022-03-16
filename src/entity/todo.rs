76199b56fc8d4261418625fe9877c2f360a5f272
loxygenk <me@loxygen.dev>
2022-03-16 12:46:55 +0900

    🐛 Create getter functions for Todo

--- src/entity/todo.rs
+++ src/entity/todo.rs
@@ -5,9 +5,20 @@ pub struct Todo {
     range: TimeRange,
     status: DoneStatusList
 }
-
 impl Todo {
-    pub fn new(task: Task, range: TimeRange, status: DoneStatusList) -> Result<Self, ()> {
-        Ok(Todo { task, range, status })
+    pub fn new(task: Task, range: TimeRange, status: DoneStatusList) -> Self {
+        Self { task, range, status }
+    }
+
+    pub fn task(&self) -> &Task {
+        &self.task
+    }
+
+    pub fn range(&self) -> &TimeRange {
+        &self.range
+    }
+
+    pub fn status(&self) -> &DoneStatusList {
+        &self.status
     }
 }
